1c30e6b4c9d5e9a9692869f308b906f325a9e3bd
stan e <yi.hang@live.com>
2018-12-12 12:00:21 +0800

    图片场景分析api增加

---
+++ src/azureai-aspnet-core/src/Yoyo.AzureAi.Application/AzureCognitive/Dtos/ImgSceneRecognitionDto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yoyo.AzureAi.AzureCognitive.Dtos
+{
+    /// <summary>
+    /// 图片场景识别Dto
+    /// </summary>
+    public class ImgSceneRecognitionDto
+    {
+        /// <summary>
+        /// 图片场景说明
+        /// </summary>
+        public List<string> Captions { get; set; }
+
+
+        /// <summary>
+        /// 标签
+        /// </summary>
+        public List<ImgSceneRecognitionTagDto> ImgTags { get; set; }
+
+
+    }
+
+    /// <summary>
+    /// 图片场景识别图片标签和匹配度
+    /// </summary>
+    public class ImgSceneRecognitionTagDto
+    {
+        /// <summary>
+        /// 标签名称
+        /// </summary>
+        public string TagName { get; set; }
+
+        /// <summary>
+        /// 百分比(保留两位小数)
+        /// </summary>
+        public double Percentage { get; set; }
+
+        /// <summary>
+        /// 百分比(例子:50.01%)
+        /// </summary>
+        public string PercentageStr { get; set; }
+    }
+}
--- src/azureai-aspnet-core/src/Yoyo.AzureAi.Web.Host/Controllers/AzureController.cs
+++ src/azureai-aspnet-core/src/Yoyo.AzureAi.Web.Host/Controllers/AzureController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 using Yoyo.AzureAi.AzureCognitive;
+using Yoyo.AzureAi.AzureCognitive.Dtos;
 using Yoyo.AzureAi.Controllers;
 
 
@@ -18,8 +20,14 @@ namespace Yoyo.AzureAi.Web.Host.Controllers
             _azureCognitiveManager = azureCognitiveManager;
         }
 
+        /// <summary>
+        /// 图片Ocr
+        /// </summary>
+        /// <param name="imgUrl">图片链接</param>
+        /// <param name="language">语言</param>
+        /// <returns>ocr识别结果</returns>
         [HttpPost]
-        public async Task<string> Ocr(string imgUrl, string language)
+        public async Task<string> ImgOcr(string imgUrl, string language)
         {
             var result = await _azureCognitiveManager.ImgOcrSimpleFormattedText(imgUrl, language);
 
@@ -27,5 +35,73 @@ namespace Yoyo.AzureAi.Web.Host.Controllers
 
         }
 
+        /// <summary>
+        /// 图片场景分析
+        /// </summary>
+        /// <param name="imgUrl">图片链接</param>
+        /// <returns>分析结果Dto</returns>
+        [HttpPost]
+        public async Task<ImgSceneRecognitionDto> ImgSceneRecognition(string imgUrl)
+        {
+            var analyzeResult = await _azureCognitiveManager.ImgAnalyze(imgUrl);
+
+            var result = new ImgSceneRecognitionDto()
+            {
+                Captions = GetCaptions(analyzeResult),
+                ImgTags = GetImgTag(analyzeResult)
+            };
+
+            return result;
+        }
+
+
+
+
+
+
+        #region 图片场景分析内部函数
+
+        /// <summary>
+        /// 获取分析结果中的图片说明
+        /// </summary>
+        /// <param name="analysis">图片分析结果</param>
+        /// <returns></returns>
+        private static List<string> GetCaptions(ImageAnalysis analysis)
+        {
+            var result = new List<string>();
+            foreach (var caption in analysis.Description.Captions)
+            {
+                result.Add(caption.Text);
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// 获取分析结果中的图片标签和百分比
+        /// </summary>
+        /// <param name="analysis">图片分析结果</param>
+        /// <returns></returns>
+        private static List<ImgSceneRecognitionTagDto> GetImgTag(ImageAnalysis analysis)
+        {
+            var result = new List<ImgSceneRecognitionTagDto>();
+
+            var tmpPercentage = 0.0;
+            foreach (var tag in analysis.Tags)
+            {
+                tmpPercentage = Math.Round(tag.Confidence * 100, 2);
+                result.Add(new ImgSceneRecognitionTagDto()
+                {
+                    TagName = tag.Name,
+                    Percentage = tmpPercentage,
+                    PercentageStr = $"{tmpPercentage}%"
+                });
+            }
+
+            return result;
+        } 
+
+        #endregion
     }
 }
